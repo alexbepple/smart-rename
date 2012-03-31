3d99607b2d27d81dd96efab083d123a6237dc517
Alex Bepple <alex@bepple.de>
2012-03-31 23:48:05 +0200

    ~ Dry spec

--- spec/smart_rename_spec.rb
+++ spec/smart_rename_spec.rb
@@ -1,12 +1,16 @@
 require './name'
 
 describe 'Smart renamer' do
-  it 'preserves a good name' do
-    good_name = 'foo (1981)  bar'
-    Name.new(good_name).clean.should == good_name
-  end
-  it 'reformats the year when the components are in the right order' do
-    good_name = 'foo (1981)  bar'
-    Name.new('foo [1981] bar').clean.should == good_name
+  data = [
+
+    'preserves good names: foo (1981)  bar',
+    'reformats year when components already in right order: foo [1981] bar',
+
+  ]
+  data.each do |datum|
+    description, example = datum.split ': '
+    it description do
+      Name.new(example).clean.should == 'foo (1981)  bar'
+    end
   end
 end
