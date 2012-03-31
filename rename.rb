5f00939993ad96c520dc3915e32bbac525663201
Alex Bepple <alex@bepple.de>
2012-03-31 23:32:25 +0200

    ~ Extract class

--- rename.rb
+++ rename.rb
@@ -1,14 +1,26 @@
-def rename original_name
-  /\w+/.match original_name
-  author = $&
-  /\d{4}/.match original_name
-  year = $&
-
-  remainder = original_name.gsub author, ''
-  remainder = remainder.gsub year, ''
-
-  /\w+/.match remainder
-  work = $&
-
-  return "#{author} (#{year})  #{work}"
+class Name < String
+  def author
+    /\w+/.match self
+    $&
+  end
+  def year
+    /\d{4}/.match self
+    $&
+  end
+  def title
+    remainder = self.remove author, year
+    /\w+/.match remainder
+    $&
+  end
+  def clean
+    "#{author} (#{year})  #{title}"
+  end
+  def remove *to_remove
+    to_remove.inject(self) {|remainder, to_remove| remainder.sub(to_remove, '')}
+  end
 end
+
+def rename original_name
+  Name.new(original_name).clean
+end
+
--- spec/smart_rename_spec.rb
+++ spec/smart_rename_spec.rb
@@ -3,10 +3,10 @@ require './rename'
 describe 'Smart renamer' do
   it 'preserves a good name' do
     good_name = 'foo (1981)  bar'
-    rename(good_name).should == good_name
+    Name.new(good_name).clean.should == good_name
   end
   it 'reformats the year when the components are in the right order' do
     good_name = 'foo (1981)  bar'
-    rename('foo [1981] bar').should == good_name
+    Name.new('foo [1981] bar').clean.should == good_name
   end
 end
