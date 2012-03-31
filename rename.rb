1c9077234f7e6c7979ff627870f2778c77c20646
Alex Bepple <alex@bepple.de>
2012-03-31 23:02:50 +0200

    + Deal with square brackets

--- rename.rb
+++ rename.rb
@@ -1,3 +1,14 @@
 def rename original_name
-  original_name
+  /\w+/.match original_name
+  author = $&
+  /\d{4}/.match original_name
+  year = $&
+
+  remainder = original_name.gsub author, ''
+  remainder = remainder.gsub year, ''
+
+  /\w+/.match remainder
+  work = $&
+
+  return "#{author} (#{year})  #{work}"
 end
--- spec/smart_rename_spec.rb
+++ spec/smart_rename_spec.rb
@@ -1,9 +1,12 @@
-
 require './rename'
 
 describe 'Smart renamer' do
   it 'preserves a good name' do
     good_name = 'foo (1981)  bar'
     rename(good_name).should == good_name
+  end
+  it 'reformats the year when the components are in the right order' do
+    good_name = 'foo (1981)  bar'
+    rename('foo [1981] bar').should == good_name
   end
 end
